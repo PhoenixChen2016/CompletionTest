4d2dbea1f640b0bcda57cd4441bfed1d2e2ddbff
Phoenix <phoenix.chen@evertrust.com.tw>
2016-09-13 13:08:38 +0800

    完成練習

---
+++ CompletionTest/TestCompletionSourceProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Utilities;
+
+namespace CompletionTest
+{
+    [Export(typeof(ICompletionSourceProvider))]
+    [ContentType("plaintext")]
+    [Name("token completion")]
+    internal class TestCompletionSourceProvider : ICompletionSourceProvider
+    {
+        [Import]
+        internal ITextStructureNavigatorSelectorService NavigatorService { get; set; }
+
+        public ICompletionSource TryCreateCompletionSource(ITextBuffer textBuffer)
+        {
+            return new TestCompletionSource(this, textBuffer);
+        }
+    }
+}
